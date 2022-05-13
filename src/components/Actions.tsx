9805854256ffcc8ab7805ecd9579764474dfae06
Deramecourt Mathurin <contact@mderam.com>
2022-05-13 20:29:29 +0200

    fix: buttons display accordingly

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -16,16 +16,36 @@ const Actions: React.FC<ActionsProps> = ({
     path,
     items
 }) => {
+    function renderActions() {
+        if (path.startsWith("/files")) {
+            return (
+                <>
+                <Upload path={path}/>
+                <Create path={path}/>
+                <Download path={path} items={items}/>
+                <Trash path={path} items={items}/>
+                </>
+            )
+        }
+        if (path.startsWith("/trash")) {
+            //TODO trashAll
+            return (
+                <>
+                <Delete path={path} items={items}/>
+                <Restore path={path} items={items}/>
+                </>
+            )
+        }
+        if (path.startsWith("/search")) {
+            return (
+                <Download path={path} items={items}/>
+            )
+        }
+    }
+
     return (
         <div className="flex border-b">
-            <Upload path={path}/>
-            <Create path={path}/>
-            <Download path={path} items={items}/>
-            {path.startsWith("/trash") && <Restore path={path} items={items}/>}
-            {path.startsWith("/trash")
-                ? <Delete path={path} items={items}/>
-                : <Trash path={path} items={items}/>
-            }
+            {renderActions()}
         </div>
     );
 }
