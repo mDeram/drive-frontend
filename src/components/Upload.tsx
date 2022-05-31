9e22b97c4ba34fbe4b108c4df3b0adc56e2c6c4c
Deramecourt Mathurin <contact@mderam.com>
2022-05-31 17:13:25 +0200

    fix: upload dropdown was removing upload inputs from the dom preventing users to upload

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -4,6 +4,7 @@ import { AiOutlineCloudUpload } from "react-icons/ai";
 import useOuterClick from "../hooks/useOuterClick";
 import { UploadContextType, useUploadContext } from "../contexts/Upload";
 import { usePathContext } from "../contexts/Path";
+import classNames from "classnames";
 
 const Upload: React.FC = () => {
     const { path } = usePathContext();
@@ -29,18 +30,18 @@ const Upload: React.FC = () => {
                 <AiOutlineCloudUpload className="text-accent-600"/>
                 Import
             </button>
-            {showDropdown &&
-                <div className="flex flex-col absolute top-full w-full border"
-                    ref={ref as any}
-                    onClick={() => setShowDropdown(false)}
-                >
-                    <label className="btn" htmlFor="upload-files" tabIndex={0}>Files</label>
-                    <input className="hidden" id="upload-files" onChange={e => upload(e.target.files)} type="file" multiple/>
+            <div className={classNames("flex flex-col absolute top-full w-full border", {
+                    "hidden": !showDropdown
+                })}
+                ref={ref as any}
+                onClick={() => setShowDropdown(false)}
+            >
+                <label className="btn" htmlFor="upload-files" tabIndex={0}>Files</label>
+                <input className="hidden" id="upload-files" onChange={e => upload(e.target.files)} type="file" multiple/>
 
-                    <label className="btn" htmlFor="upload-folder" tabIndex={0}>Folder</label>
-                    <input className="hidden" id="upload-folder" onChange={e => upload(e.target.files)} type="file" webkitdirectory="" directory=""/>
-                </div>
-            }
+                <label className="btn" htmlFor="upload-folder" tabIndex={0}>Folder</label>
+                <input className="hidden" id="upload-folder" onChange={e => upload(e.target.files)} type="file" webkitdirectory="" directory=""/>
+            </div>
         </div>
     )
 }
